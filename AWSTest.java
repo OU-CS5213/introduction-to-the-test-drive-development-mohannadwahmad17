24a3fa14c2ba1b65fd956dc1bb696f966dc8d6b5
mohannadwahmad17 <mohannadwahmad97@gmail.com>
2021-03-02 16:40:24 -0600

    added test code to testGetValues() that fails

--- AWSTest.java
+++ AWSTest.java
@@ -16,7 +16,14 @@ class AWSTest {
 
 	@Test
 	void testGetValues() {
-		fail("Not yet implemented");
+		int[] expected = {1, 2, 3};
+		int[] x = {1, 3, 2};
+		
+		AWS aws = new AWS(x);
+		
+		int[] actual = aws.getValues();
+		assertEquals(actual[0], expected[0]);
+		assertEquals(actual[1], expected[1]);
 	}
 
 	@Test
